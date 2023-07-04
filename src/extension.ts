e2573833711b36a8c1dc3475ab11c98e91090fd7
joesobo <soboleskijoe@gmail.com>
2023-07-04 14:05:38 -0700

    convert to reading connection at a time

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,33 @@
 import * as vscode from 'vscode'
 
-import type { File } from './utils/types'
-import { getConnections } from './utils/file/getConnections'
+import type { Edge, File, Node } from './utils/types'
+
+import { getConnection } from './utils/file/getConnections'
 
 export interface Data {
   message: File[]
+}
+
+export interface PluginData {
+	name: string
+	regex: RegExp[]
+	getConnection: (match: RegExpExecArray, file: File, fileIndex: number, nodes: Node[], edges: Edge[]) => { nodes: Node[]; edges: Edge[] }
 }
 
 export const activate = async (context: vscode.ExtensionContext) => {
 	// eslint-disable-next-line no-console -- For Testing Purposes
 	console.log('CodeGraphy - JS Plugin activated!')
 
-	const data: Data = await vscode.commands.executeCommand(
-		'codegraphy.getSystemFiles',
+	await vscode.commands.executeCommand(
+		'codegraphy.registerPlugin',
+		{
+			name: 'JSPlugin',
+			regex: [
+				/import\s+(?:\*+\s+as\s+[\w*]+|(?:type\s+(?:(?:[\w*]+\s*,\s*)?\{[^{}]*\})+|(?:(?:[\w*]+\s*,\s*)?\{[^{}]*\}|[\w*]+)))\s*from\s*['"]([^'"]+)['"]/g,
+				/require\s*\(\s*['"]([^'"]+)['"]\s*\)/g,
+				/.*CodeGraphy\s+connect:\s+(['"])([^'"]+)\1/g,
+			],
+			getConnection,
+		},
 	)
-	if (data) {
-		const files = data.message
-
-		const { nodes, edges } = getConnections(files)
-
-		const result = {
-			nodes,
-			edges,
-		}
-		await vscode.commands.executeCommand(
-			'codegraphy.sendPluginConnections',
-			result,
-		)
-	}
 }
--- src/utils/file/getConnections.ts
+++ src/utils/file/getConnections.ts
@@ -1,69 +1,38 @@
-import fs from 'node:fs'
 import type { Edge, File, Node } from '../types'
 import { findNearestNodeModules, isNodeModule } from './getNodeModules'
 
-export const getConnections = (files: File[]) => {
-	const nodes: Node[] = []
-	const edges: Edge[] = []
+export const getConnection = (match: RegExpExecArray, file: File, fileIndex: number, nodes: Node[], edges: Edge[]) => {
+	const tempNodes: Node[] = []
+	const tempEdges: Edge[] = []
+	const importPath = match[1]
+	const fullPath = getFullPath(file.path, importPath)
 
-	files.forEach((file, fileIndex) => {
-		nodes.push({
-			id: fileIndex,
-			label: file.path.split('/').pop() || '',
-			path: file.path,
-			type: 'File',
+	if (isNodeModule(fullPath, nodes)) {
+		tempNodes.push({
+			id: nodes.length,
+			label: fullPath.split('/').pop() || '',
+			path: fullPath,
+			type: 'Package',
 		})
+	}
+
+	const connectionIndex = findConnectionIndex(nodes, fullPath)
+
+	// If the connection already exists or no connection found, skip it
+	if (
+		connectionIndex === -1
+		|| edges.find(
+			connection => connection.id === `${fileIndex}-${connectionIndex}`,
+		)
+	) { return { nodes: tempNodes, edges: tempEdges } }
+
+	tempEdges.push({
+		id: `${fileIndex}-${connectionIndex}`,
+		to: fileIndex,
+		from: connectionIndex,
 	})
 
-	files.forEach((file, fileIndex) => {
-		const fileContents = fs.readFileSync(file.path, 'utf-8')
-
-		const importRegex
-      = /(?:import\s+(?:\*+\s+as\s+[\w*]+|(?:type\s+(?:(?:[\w*]+\s*,\s*)?\{[^{}]*\})+|(?:(?:[\w*]+\s*,\s*)?\{[^{}]*\}|[\w*]+)))\s*from\s*['"]([^'"]+)['"]|require\s*\(\s*['"]([^'"]+)['"]\s*\)|.*CodeGraphy\s+connect:\s+(['"])([^'"]+)\3)/g
-
-		let match
-
-		do {
-			match = importRegex.exec(fileContents)
-
-			if (match === null) {
-				break
-			}
-
-			// If it's an import statement, the path is captured in the second group.
-			// If it's a require statement, the path is captured in the third group.
-			// If it's a custom "CodeGraphy connect" comment, the path is captured in the fourth group.
-			const importPath = match[1] || match[2] || match[4]
-			const fullPath = getFullPath(file.path, importPath)
-
-			if (isNodeModule(fullPath, nodes)) {
-				nodes.push({
-					id: nodes.length,
-					label: fullPath.split('/').pop() || '',
-					path: fullPath,
-					type: 'Package',
-				})
-			}
-
-			const connectionIndex = findConnectionIndex(nodes, fullPath)
-
-			// If the connection already exists or no connection found, skip it
-			if (
-				connectionIndex === -1
-        || edges.find(
-        	connection => connection.id === `${fileIndex}-${connectionIndex}`,
-        )
-			) { continue }
-
-			edges.push({
-				id: `${fileIndex}-${connectionIndex}`,
-				to: fileIndex,
-				from: connectionIndex,
-			})
-		} while (match !== null)
-	})
-
-	return { nodes, edges }
+	return { nodes: tempNodes, edges: tempEdges }
 }
 
 // Get full path of import
